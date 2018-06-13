769db36a9a78c4690f5d518009adbdc25a8415cc
Shai Ben Or <shaivlaid@gmail.com>
2018-06-13 13:14:51 +0300

    document testkit

--- src/AddItem/AddItem.driver.js
+++ src/AddItem/AddItem.driver.js
@@ -10,12 +10,19 @@ const addItemDriverFactory = ({wrapper, element}) => {
   const addTooltip = () => byHook('add-tooltip');
 
   return {
+    /** return the driver element */
     element: () => element,
+    /** return the element's height */
     getHeight: () => window.getComputedStyle(element).height,
+    /** return the element's width */
     getWidth: () => window.getComputedStyle(element).width,
+    /** return true if tooltip is visible */
     isAddButtonVisible: () => !!addImageButton(),
+    /** return true if button is visible */
     isAddTooltipVisible: () => !!addTooltip(),
+    /** click the add button */
     click: () => ReactTestUtils.Simulate.click(addImageButton()),
+    /** get the Tooltip's driver */
     getTooltipDriver: () => tooltipTestkitFactory({wrapper, dataHook: 'add-tooltip'}),
     exists: () => !!element
   };
