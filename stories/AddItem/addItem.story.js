2fe3555b3d7756858a80386d9d3ef56f63ea1473
Shai Ben Or <shaivlaid@gmail.com>
2018-06-17 10:26:51 +0300

    Fix story props

--- stories/AddItem/addItem.story.js
+++ stories/AddItem/addItem.story.js
@@ -10,7 +10,7 @@ export default {
   componentPath: '../../src/AddItem',
 
   componentProps: () => ({
-    onAddItem: () => {},
+    onClick: () => {},
     examples: <Example/>,
     dataHook: 'storybook-addItem'
   })
