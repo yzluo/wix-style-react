8759b8ba05d5732d7c3a613fdfe9c93c9400cc62
Erez Makavy <erez@wix.com>
2018-07-23 12:39:18 +0300

    Add backward compatible import for TableToolbar with deprecation logs

---
+++ src/Table/Toolbar/TableToolbar.js
@@ -0,0 +1,4 @@
+import deprecationLog from '../../utils/deprecationLog';
+
+deprecationLog('Importing TableToolbar from wix-style-react/dist/src/components/Table/Toolbar/TableToolbar is deprecated. Please import from wix-style-react/TableToolbar');
+export * from '../TableToolbar/TableToolbar';
---
+++ src/Table/Toolbar/index.js
@@ -0,0 +1,4 @@
+import deprecationLog from '../../utils/deprecationLog';
+
+deprecationLog('Importing TableToolbar from wix-style-react/dist/src/components/Table/Toolbar is deprecated. Please import from wix-style-react/TableToolbar');
+export * from '../TableToolbar';
