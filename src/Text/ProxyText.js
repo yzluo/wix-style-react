cd619a0afbefb10fcebced94bdf1aaa1a6bdf16f
Lior Belinsky <liorbe@wix.com>
2018-07-25 14:02:19 +0300

    use core HOC

--- src/Text/ProxyText.js
+++ src/Text/ProxyText.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {bool} from 'prop-types';
 import {createHOC} from 'wix-ui-core/dist/src/createHOC';
 import Text from './Text';
-import {withEllipsedTooltip} from '../common/EllipsedTooltip/core';
-import style from '../common/EllipsedTooltip/EllipsedTooltip.st.css';
+import {withEllipsedTooltip} from 'wix-ui-core/dist/src/hocs/EllipsedTooltip';
+import ellipsedStyle from './EllipsedTooltip.st.css';
 
 const EllipsedText = withEllipsedTooltip({showTooltip: true})(Text);
 
 const ProxyText = ({ellipsis, ...props}) => ellipsis ?
-  <EllipsedText {...style('root', {}, props)} {...props}/> :
+  <EllipsedText {...ellipsedStyle('root', {}, props)} {...props}/> :
   <Text {...props}/>;
 
 ProxyText.propTypes = {
