ab9530a9448975c96d8154f16a485c0b97930345
Faradey27 <andriit@wix.com>
2018-08-17 13:53:39 +0300

    Add regression test for PopoverMenu error

--- src/PopoverMenu/PopoverMenu.e2e.js
+++ src/PopoverMenu/PopoverMenu.e2e.js
@@ -1,3 +1,4 @@
+import browserLogs from 'protractor-browser-logs';
 import {
   POPOVER_MENU_DATA_HOOK,
   POPOVER_MENU_ITEM_DATA_HOOK
@@ -12,12 +13,18 @@ import {getStoryUrl} from '../../test/utils/storybook-helpers';
 describe('PopoverMenu', () => {
   const storyUrl = getStoryUrl('7. Tooltips', '7.3. Popover Menu');
   let driver;
+  const logs = browserLogs(browser);
 
   beforeEach(() => {
+    logs.reset();
+    logs.ignore(message => message.message.indexOf('Uncaught') === -1);
+
     driver = popoverMenuTestkitFactory({dataHook: POPOVER_MENU_DATA_HOOK})
       .init.menuItemDataHook(POPOVER_MENU_ITEM_DATA_HOOK);
     browser.get(storyUrl);
   });
+
+  afterEach(() => logs.verify());
 
   it('should show popover menu', () => {
     waitForVisibilityOf(driver.element(), 'Can not find PopoverMenu trigger element').then(() => {
@@ -40,6 +47,38 @@ describe('PopoverMenu', () => {
           'PopoverMenu has not been hidden after menu item click',
         );
       });
+    });
+  });
+
+  describe('regression tests', () => {
+    it('Uncaught TypeError: https://github.com/wix/wix-style-react/issues/2113', async () => {
+      const storyUrl = getStoryUrl('Tests/7. Tooltip', '7.3. Popover Menu');
+      await browser.get(storyUrl);
+
+      driver = popoverMenuTestkitFactory({dataHook: 'popover-0'})
+        .init.menuItemDataHook('popover-item-0');
+
+      await waitForVisibilityOf(driver.element());
+
+      driver.click();
+
+      await waitForVisibilityOf(driver.menu.element());
+
+      driver.menu.clickItemAt(0);
+
+      await browser.wait(
+        EC.stalenessOf(driver.menu.element()),
+        5000,
+        'PopoverMenu has not been hidden after menu item click',
+      );
+
+      await browser.wait(
+        EC.stalenessOf(driver.element()),
+        5000,
+        'PopoverMenu has not been removed after menu item click',
+      );
+
+      return logs.expect(/Uncaught TypeError: Cannot read property 'parentElement' of null/);
     });
   });
 });
