1f4bdc1f9536f60c9060739794db46f7aec43058
Shlomi Toussia-Cohen <shlomitc@wix.com>
2018-09-02 16:48:11 +0300

    remove private eys

--- test/protractor-register.js
+++ test/protractor-register.js
@@ -1,13 +1,6 @@
 require('regenerator-runtime/runtime');
 const sass = require('node-sass');
 const {wixCssModulesRequireHook} = require('yoshi-runtime');
-const {tryRequire} = require('yoshi/src/utils');
-
-tryRequire('../private/node_modules/wix-eyes-env');
-
-// Private Wix environment config for screenshot reporter
-// Read how to set your own params (if needed) here: https://github.com/wix/screenshot-reporter#usage
-tryRequire('../private/node_modules/screenshot-reporter-env');
 
 require('yoshi/src/require-hooks');
 
