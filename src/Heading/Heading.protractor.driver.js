6d7b632cfec8c69c00ad500e0660b9850790a4e6
Lior Belinsky <liorbe@wix.com>
2018-10-31 17:38:59 +0200

    migrate heading protractor driver to unidriver

--- src/Heading/Heading.protractor.driver.js
+++
@@ -1,8 +0,0 @@
-export const headingDriverFactory = component => ({
-  /** returns the component element */
-  element: () => component,
-  /** returns the component's text */
-  getText: async () => component.getText()
-});
-
-export default headingDriverFactory;
