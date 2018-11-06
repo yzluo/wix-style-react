9f487241bdeea01c04c83aeb60f39e119d2d5e00
Ehab Sharif <ehabs@wix.co>
2018-11-06 12:42:04 +0200

    add HeaderLayout for ComposerLayout

---
+++ src/ComposerLayout/HeaderLayout.js
@@ -0,0 +1,35 @@
+import React, {PropTypes} from 'react';
+import {Row} from './Row';
+import XIcon from 'wix-style-react/new-icons/X';
+import InfoIcon from 'wix-style-react/new-icons/Info';
+
+export const HeaderLayout = props => {
+  const rightAlignedItems = (<div>
+    {props.showQuestionMarkButton && <InfoIcon data-hook="info-icon" onClick={props.onClickQuestionMark}/>}
+    {props.showCloseButton && <XIcon data-hook="x-icon" onClick={props.onClickX}/>}
+  </div>);
+  return (
+    <Row
+      rightAlignedItems={rightAlignedItems}
+      centerAlignedItems={props.title}
+      leftAlignedItems={props.secondaryItems}
+      />
+  );
+}
+;
+
+
+HeaderLayout.propTypes = {
+  showCloseButton: PropTypes.bool,
+  showQuestionMarkButton: PropTypes.bool,
+  onClickX: PropTypes.func,
+  onClickQuestionMark: PropTypes.func,
+  title: PropTypes.string,
+  secondaryItems: PropTypes.node
+};
+
+HeaderLayout.defaultProps = {
+  showCloseButton: true,
+  showQuestionMarkButton: true
+}
+;
