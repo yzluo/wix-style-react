3f371c5fb98d81d0a4f8bac89cdd2e394f9c9434
Erez Makavy <erezm@wix.com>
2018-11-08 14:13:10 +0200

    Remove unecessary async from describe functions

--- src/Page/Page.e2e.js
+++ src/Page/Page.e2e.js
@@ -18,7 +18,7 @@ const { category, storyName } = storySettings;
 const testStoryUrl = testName =>
   createTestStoryUrl({ category, storyName, testName });
 
-describe('Page', async () => {
+describe('Page', () => {
   const dataHook = 'story-page';
 
   const initTest = async ({ storyUrl, dataHook, props }) => {
@@ -47,12 +47,13 @@ describe('Page', async () => {
     });
   };
 
-  describe('Header + Tail + Content', async () => {
+  describe('Header + Tail + Content', () => {
     const storyUrl = createStoryUrl({
       kind: category,
       story: storyName,
       withExamples: false,
     });
+
     describe('With Background-Image', () => {
       runTestCases({ storyUrl, dataHook });
     });
@@ -62,7 +63,7 @@ describe('Page', async () => {
     });
   });
 
-  describe('Header + Content', async () => {
+  describe('Header + Content', () => {
     describe('With Background-Image', () => {
       const storyUrl = testStoryUrl('1. Image');
       runTestCases({ storyUrl, dataHook });
