f4b570343884e6305d9306f3dbbca642005f5014
Erez Makavy <erezm@wix.com>
2018-11-08 11:41:58 +0200

    Fix ImageViewer.spec - use dom-test-container

--- src/ImageViewer/ImageViewer.spec.js
+++ src/ImageViewer/ImageViewer.spec.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ImageViewer from './ImageViewer';
 import ImageViewerDriverFactory from './ImageViewer.driver';
-import { createDriverFactory } from 'wix-ui-test-utils/driver-factory';
+
+import { ReactDOMTestContainer } from "../../test/dom-test-container";
 
 describe('ImageViewer', () => {
-  const createDriver = createDriverFactory(ImageViewerDriverFactory);
+  const createDriver = new ReactDOMTestContainer()
+    .unmountAfterEachTest()
+    .createLegacyRenderer(ImageViewerDriverFactory);
+
   let props, driver;
   const IMAGE_URL = 'some-image-url.png';
   const addImage = jest.fn();
