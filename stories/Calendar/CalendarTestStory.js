52604f37589ddb34e23ce99f8f86808f638f5c81
Erez Makavy <erezm@wix.com>
2018-11-12 14:06:45 +0200

    Add calendar test page and e2e test accordingly

--- stories/Calendar/CalendarTestStory.js
+++ stories/Calendar/CalendarTestStory.js
@@ -2,31 +2,33 @@ import React from 'react';
 import queryString from 'query-string';
 import pick from 'lodash/pick';
 
-import {storiesOf} from '@storybook/react';
-import {getTestStoryKind, Category} from '../storiesHierarchy';
+import { storiesOf } from '@storybook/react';
+import { getTestStoryKind, Category } from '../storiesHierarchy';
 import Calendar from 'wix-style-react/Calendar';
 
+storiesOf(
+  getTestStoryKind({ category: Category.INPUTS, storyName: '3.13 Calendar' }),
+  module,
+).add('1. selectedDays', () => {
+  const selectedDaysParam = pick(
+    queryString.parse(window.location.search),
+    'selectedDays',
+  );
 
-storiesOf(getTestStoryKind({category: Category.INPUTS, storyName: '3.13 Calendar'}), module)
-  .add('1. selectedDays', () => {
-    const selectedDaysParam = pick(queryString.parse(window.location.search), 'selectedDays');
-
-    const obj = selectedDaysParam.selectedDays ? JSON.parse(selectedDaysParam.selectedDays) : JSON.parse(JSON.stringify(new Date()));
-    let selectedDays;
-    if (typeof obj === 'object') {
-      selectedDays = {};
-      selectedDays.from = new Date(Date.parse(obj.from));
-      selectedDays.to = new Date(Date.parse(obj.to));
-    } else {
-      selectedDays = new Date(Date.parse(obj));
-    }
-    return (
-      <div>
-        <Calendar
-          dataHook="calendar"
-          selectedDays={selectedDays}
-          />
-      </div>
-    );
+  const obj = selectedDaysParam.selectedDays
+    ? JSON.parse(selectedDaysParam.selectedDays)
+    : JSON.parse(JSON.stringify(new Date()));
+  let selectedDays;
+  if (typeof obj === 'object') {
+    selectedDays = {};
+    selectedDays.from = new Date(Date.parse(obj.from));
+    selectedDays.to = new Date(Date.parse(obj.to));
+  } else {
+    selectedDays = new Date(Date.parse(obj));
   }
-);
+  return (
+    <div>
+      <Calendar dataHook="calendar" selectedDays={selectedDays} />
+    </div>
+  );
+});
