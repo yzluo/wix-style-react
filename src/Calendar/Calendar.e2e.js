68ead851c00b8d5d92ddf574f7bc86d3ace0b693
Erez Makavy <erezm@wix.com>
2018-11-12 14:06:45 +0200

    Add calendar test page and e2e test accordingly

--- src/Calendar/Calendar.e2e.js
+++ src/Calendar/Calendar.e2e.js
@@ -1,44 +1,28 @@
 import eyes from 'eyes.it';
-import {calendarTestkitFactory} from '../../testkit/protractor';
-import {getStoryUrl} from '../../test/utils/storybook-helpers';
-import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver';
+import {calendarTestkitFactory, waitForVisibilityOf} from '../../testkit/protractor';
+import {craeteStoryUrl} from '../../test/utils/storybook-helpers';
+import {TESTS_PREFIX} from '../../stories/storiesHierarchy';
 
 describe('Calendar', () => {
+  const storyUrl = ({selectedDays}) => {
+    const baseUrl = craeteStoryUrl({kind: `${TESTS_PREFIX}/3. Inputs/3.13 Calendar`, story: '1. selectedDays'});
+    return `${baseUrl}&selectedDays=${JSON.stringify(selectedDays)})}`;
+  };
   const dataHook = 'calendar';
-  const storyUrl = getStoryUrl('3. Inputs', '3.13 Calendar');
   const driver = calendarTestkitFactory({dataHook});
 
-  beforeAll(() => {
-    browser.get(storyUrl);
-  });
+  eyes.it('should render with a selected day', async () => {
+    await browser.get(storyUrl({selectedDays: new Date('2017/05/01').stringify()}));
+    await waitForVisibilityOf(driver.element(), 'Cannot find Calendar');
+  }, {enableSnapshotAtBrowserGet: false});
 
-  beforeEach(() => {
-    autoExampleDriver.reset();
-  });
-
-  describe('default', () => {
-    eyes.it('should not break design', async () => {
-      autoExampleDriver.setProps({value: new Date('2017/05/01')});
-      expect(await driver.exists()).toBe(true);
-      await eyes.checkWindow(dataHook);
-    });
-  });
-
-  describe('selectedDays', () => {
-    eyes.it('should work for a single date', async () => {
-      autoExampleDriver.setProps({selectedDays: new Date('2017/05/01')});
-      expect(await driver.exists()).toBe(true);
-      expect(await driver.getSelectedDays()).toEqual(['1']);
-      await eyes.checkWindow(dataHook);
-    });
-    
-    // doesn't work because protractor fails to support {from:Date,to:Date} props
-    /*eyes.it('should work for a date range', async () => {
-      autoExampleDriver.setProps({selectedDays: {from: new Date('2017/05/02'), to: new Date('2017/05/06')}});
-      expect(await driver.exists()).toBe(true);
-      //expect(await driver.getSelectedDays()).toEqual(['1']);
-      browser.sleep(100000);
-      await eyes.checkWindow(dataHook);
-    });*/
-  });
+  eyes.it('should render with a selected range', async () => {
+    await browser.get(storyUrl({
+      selectedDays: {
+        from: new Date('2017/05/02').stringify(),
+        to: new Date('2017/05/06').stringify()
+      }}));
+    await waitForVisibilityOf(driver.element(), 'Cannot find Calendar');
+    expect(await driver.getSelectedDays()).toEqual(['1']);
+  }, {enableSnapshotAtBrowserGet: false});
 });
