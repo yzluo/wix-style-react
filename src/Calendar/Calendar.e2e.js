4651ef7b93e26cb7a70cf763ca6d36edf0f71bc2
Tal Mardix <talma@wix.com>
2018-11-13 16:33:05 +0200

    .

--- src/Calendar/Calendar.e2e.js
+++ src/Calendar/Calendar.e2e.js
@@ -12,15 +12,16 @@ describe('Calendar', () => {
   const driver = calendarTestkitFactory({dataHook});
 
   eyes.fit('should render with a selected day', async () => {
-    await browser.get(storyUrl({selectedDays: JSON.stringify(new Date('2017/05/01'))}));
-    await waitForVisibilityOf(driver.exists(), 'Cannot find Calendar');
+    console.log('---', storyUrl({selectedDays: new Date('2017/05/01')}))
+    await browser.get(storyUrl({selectedDays: new Date('2017/05/01')}));
+    await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
   }, {enableSnapshotAtBrowserGet: false});
 
   eyes.it('should render with a selected range', async () => {
     await browser.get(storyUrl({
       selectedDays: {
-        from: JSON.stringify(new Date('2017/05/02')),
-        to: JSON.stringify(new Date('2017/05/06'))
+        from: new Date('2017/05/02'),
+        to: new Date('2017/05/06')
       }}));
     await waitForVisibilityOf(driver.element(), 'Cannot find Calendar');
     expect(await driver.getSelectedDays()).toEqual(['1']);
