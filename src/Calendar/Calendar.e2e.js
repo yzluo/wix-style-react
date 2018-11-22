9db438188c20c216c72d43c64480213f695f280d
Erez Makavy <erezm@wix.com>
2018-11-22 17:16:09 +0200

    MIgreate to Yoshi 3
    Fix Page story - remove usage of spread Array (+13 squashed commits)
    Squashed commits:
    [] - protractor - fix duplicate import. Typography - fix disable no-restricted-globals
    [7e384ff] Skip deprecated backoffice tooltip test case
    [9d554b2] Fix Button e2e - remove use of propTypes
    [68a4b76] Fix Array spread
    [d2d8eaf] Fix Calendar e2e
    [48187b7] Fix FocusableHOC propTypes removal with hack
    [7d6337b] Remove unecessary async from describe calls
    [bb35c60] Fix ImageViewer.spec - use dom-test-container
    [76c7282] Fix GoogleMapsClient spec
    [2b93aa1] Fix DataTable spec - expect should not receive a 2nd argument
    [cb5030f] Fix tests with missing asset prefix (http localhost)
    [8998a0c] Fix Typography and ExampleBadges
    [88454c3] Fix eslint jest/no-identical-title errors
    [b2b96eb] package json remove features (+1 squashed commit)
    Squashed commits:
    [008557a] Apply eslint config changes
    
    revert babel preset to old configureation (dont use preset yoshi)

--- src/Calendar/Calendar.e2e.js
+++ src/Calendar/Calendar.e2e.js
@@ -1,52 +1,92 @@
 import eyes from 'eyes.it';
-import {calendarTestkitFactory, waitForVisibilityOf} from '../../testkit/protractor';
-import {createStoryUrl} from '../../test/utils/storybook-helpers';
-import {TESTS_PREFIX} from '../../stories/storiesHierarchy';
+import {
+  calendarTestkitFactory,
+  waitForVisibilityOf,
+} from '../../testkit/protractor';
+import { createStoryUrl } from '../../test/utils/storybook-helpers';
+import { TESTS_PREFIX } from '../../stories/storiesHierarchy';
 
 describe('Calendar', () => {
-  const storyUrl = ({selectedDays}) => {
-    const baseUrl = createStoryUrl({kind: `${TESTS_PREFIX}/3. Inputs/3.13 Calendar`, story: '1. selectedDays'});
-    return `${baseUrl}&selectedDays=${global.encodeURIComponent(JSON.stringify(selectedDays))}`;
+  const storyUrl = ({ selectedDays }) => {
+    const baseUrl = createStoryUrl({
+      kind: `${TESTS_PREFIX}/3. Inputs/3.13 Calendar`,
+      story: '1. selectedDays',
+    });
+    return `${baseUrl}&selectedDays=${global.encodeURIComponent(
+      JSON.stringify(selectedDays),
+    )}`;
   };
   const dataHook = 'calendar';
-  const driver = calendarTestkitFactory({dataHook});
+  const driver = calendarTestkitFactory({ dataHook });
 
-  eyes.it('should correctly render when selectedDays is a single date', async () => {
-    await browser.get(storyUrl({selectedDays: new Date('2017/05/01')}));
-    await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
-  }, {enableSnapshotAtBrowserGet: false});
+  eyes.it(
+    'should correctly render when selectedDays is a single date',
+    async () => {
+      await browser.get(storyUrl({ selectedDays: new Date('2017/05/01') }));
+      await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
+    },
+    { enableSnapshotAtBrowserGet: false },
+  );
 
-  eyes.it('should correctly render when selectedDays is a date range', async () => {
-    await browser.get(storyUrl({
-      selectedDays: {
-        from: new Date('2017/05/02'),
-        to: new Date('2017/05/06')
-      }}));
-    await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
-  }, {enableSnapshotAtBrowserGet: false});
+  eyes.it(
+    'should correctly render when selectedDays is a date range',
+    async () => {
+      await browser.get(
+        storyUrl({
+          selectedDays: {
+            from: new Date('2017/05/02'),
+            to: new Date('2017/05/06'),
+          },
+        }),
+      );
+      await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
+    },
+    { enableSnapshotAtBrowserGet: false },
+  );
 
-  eyes.it('should correctly render when selectedDays is an infinite date range starting in the current month', async () => {
-    await browser.get(storyUrl({
-      selectedDays: {
-        from: new Date('2017/05/02')
-      }}));
-    await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
-  }, {enableSnapshotAtBrowserGet: false});
+  eyes.it(
+    'should correctly render when selectedDays is an infinite date range starting in the current month',
+    async () => {
+      await browser.get(
+        storyUrl({
+          selectedDays: {
+            from: new Date('2017/05/02'),
+          },
+        }),
+      );
+      await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
+    },
+    { enableSnapshotAtBrowserGet: false },
+  );
 
-  eyes.it('should correctly render when selectedDays is an infinite date range ending in the current month', async () => {
-    await browser.get(storyUrl({
-      selectedDays: {
-        to: new Date('2017/05/02')
-      }}));
-    await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
-  }, {enableSnapshotAtBrowserGet: false});
+  eyes.it(
+    'should correctly render when selectedDays is an infinite date range ending in the current month',
+    async () => {
+      await browser.get(
+        storyUrl({
+          selectedDays: {
+            to: new Date('2017/05/02'),
+          },
+        }),
+      );
+      await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
+    },
+    { enableSnapshotAtBrowserGet: false },
+  );
 
-  eyes.it('should correctly render when selectedDays is aa range of one day', async () => {
-    await browser.get(storyUrl({
-      selectedDays: {
-        from: new Date('2017/05/02'),
-        to: new Date('2017/05/02')
-      }}));
-    await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
-  }, {enableSnapshotAtBrowserGet: false});
+  eyes.it(
+    'should correctly render when selectedDays is aa range of one day',
+    async () => {
+      await browser.get(
+        storyUrl({
+          selectedDays: {
+            from: new Date('2017/05/02'),
+            to: new Date('2017/05/02'),
+          },
+        }),
+      );
+      await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
+    },
+    { enableSnapshotAtBrowserGet: false },
+  );
 });
