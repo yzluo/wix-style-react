3e141cbc4e4d68226c90f6841bcf973dc4e4d673
mykas <mykolass@wix.com>
2018-11-22 15:00:24 +0200

    Remove some comments

--- test/utils/vanilla-uni-helpers.js
+++ test/utils/vanilla-uni-helpers.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import {Simulate} from 'react-dom/test-utils';
 import {reactUniDriver} from 'unidriver';
 
-// At the moment our tests support both Jsdom and browser environment.
-// The browser test runner provides #root element to render into, and
-// in Jsdom we're going to add the container into the body.
 export function createDOMContainer() {
   const root = document.querySelector('#root') || document.body;
   const div = document.createElement('div');
