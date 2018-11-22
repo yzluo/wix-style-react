0cd1e5b0d66a7722c0412c7399e8deea20e25d0e
mykas <mykolass@wix.com>
2018-11-22 16:50:30 +0200

    Testing

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,17 @@
+const {retry} = require('protractor-retry');
+
 module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
   jasmineNodeOpts: {defaultTimeoutInterval: 60000},
+  onCleanUp(results) {
+    retry.onCleanUp(results);
+  },
   onPrepare() {
+    retry.onPrepare();
     browser.ignoreSynchronization = true;
+  },
+  afterLaunch() {
+    return retry.afterLaunch(2); // number of retries ( default is 2 )
   }
 };
