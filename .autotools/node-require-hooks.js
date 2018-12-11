d462850c4ad488528508bff2fdb9b0f5b4e00bd2
Erez Makavy <erezm@wix.com>
2018-12-11 15:33:41 +0200

    Fix require hooks

--- .autotools/node-require-hooks.js
+++ .autotools/node-require-hooks.js
@@ -1,6 +1,3 @@
-console.log('hooks');
-// module.exports = () => {
-  require('yoshi/src/require-hooks');
-  require('yoshi-runtime').wixCssModulesRequireHook('./src');
-  require('@stylable/node').attachHook();
-// };
+require('yoshi/src/require-hooks');
+require('yoshi-runtime').wixCssModulesRequireHook('./src');
+require('@stylable/node').attachHook();
