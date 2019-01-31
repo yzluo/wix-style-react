8aa69401d266a0afe28695747e7ea1f6af00d701
Artem Yavorsky <aqson@me.com>
2019-01-31 12:26:28 +0200

    Use progress bar for transpile process.

--- scripts/transpile.js
+++ scripts/transpile.js
@@ -1,22 +1,44 @@
 /* eslint no-console: 0 */
 
 const execa = require('execa');
+const ProgressBar = require('progress');
 
-const options = { stdio: 'inherit' };
-
-const testkit = execa.shell(
-  'babel testkit --out-dir dist/testkit --copy-files --plugins=babel-plugin-transform-es2015-modules-commonjs',
+const STEP_WIDTH = 9;
+const STEPS = 4;
+const progress = new ProgressBar(
+  'Transpiling `src` -> `dist` :bar :percent :dir',
   {
-    ...options,
+    total: STEP_WIDTH * STEPS,
   },
 );
 
-const stories = execa.shell(
-  'babel stories --out-dir dist/stories --copy-files --plugins=babel-plugin-transform-es2015-modules-commonjs',
-  {
-    ...options,
-  },
-);
+const options = { stdio: 'pipe', env: { FORCE_COLOR: true } };
+
+const testkit = execa
+  .shell(
+    'babel testkit --out-dir dist/testkit --copy-files --plugins=babel-plugin-transform-es2015-modules-commonjs',
+    {
+      ...options,
+    },
+  )
+  .then(() => {
+    progress.tick(STEP_WIDTH, {
+      dir: 'testkit',
+    });
+  });
+
+const stories = execa
+  .shell(
+    'babel stories --out-dir dist/stories --copy-files --plugins=babel-plugin-transform-es2015-modules-commonjs',
+    {
+      ...options,
+    },
+  )
+  .then(() => {
+    progress.tick(STEP_WIDTH, {
+      dir: 'stories',
+    });
+  });
 
 const sources = (async () => {
   await execa.shell(
@@ -25,18 +47,21 @@ const sources = (async () => {
       ...options,
     },
   );
+  progress.tick(STEP_WIDTH, {
+    dir: 'es6',
+  });
   await execa.shell(
     'babel dist/es/src --out-dir dist/src --copy-files --plugins=babel-plugin-transform-es2015-modules-commonjs --no-babelrc',
     {
       ...options,
     },
   );
+  progress.tick(STEP_WIDTH, {
+    dir: 'es5',
+  });
 })();
 
-Promise.all([testkit, stories, sources])
-  .then(() => {
-    console.log('Done!');
-  })
-  .catch(error => {
-    console.warn(error);
-  });
+Promise.all([testkit, stories, sources]).catch(error => {
+  progress.interrupt('Error');
+  return Promise.reject(error);
+});
